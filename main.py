b055a78e2e1cce4591a66afea388757809b212e1
eddabbah mohamed <eddabbah@gmail.com>
2023-11-04 22:39:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-import logging
-import logging.handlers
-import os
-
 import requests
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
-if __name__ == "__main__":
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+if r.status_code == 200:
         data = r.json()
         temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+font = ImageFont.truetype("Gidole-Regular.ttf", size=50)
+img = Image.open("meteoback.jpg")
+img_cat=Image.open("meteoback.jpg")
+draw = ImageDraw.Draw(img)
+D0=90
+L=300
+draw.text((D0, L), str(temperature),font=font, fill='black')
+img.paste(img_cat,(200, 200),)
+img = img.save("newmeteo.jpg") 
