22fb41ed956b7693892af7c913a284e8a4e22f55
eddabbah mohamed <eddabbah@gmail.com>
2023-11-06 20:01:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-import requests
-from PIL import Image, ImageDraw, ImageFont, ImageColor
-r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-font = ImageFont.truetype("Gidole-Regular.ttf", size=50)
-img = Image.open("meteoback.jpg")
-img_cat=Image.open("meteoback.jpg")
-draw = ImageDraw.Draw(img)
-D0=90
-L=300
-draw.text((D0, L), str(temperature),font=font, fill='black')
-img.paste(img_cat,(200, 200),)
-img = img.save("newmeteo.jpg") 
+# Writing to a file in Python
+# This was tested on Python 2.7.
+
+# path - is a string to desired path location. The file does
+#         not have to exist.
+# content - is a string with the file content.
+def writeToFile(path, content):
+  file = open(path, "w")
+  file.write(content)
+  file.close()
+  
+  
+PATH_TO_MY_FILE = './example.txt'
+CONTENT_FOR_MY_FILE = 'Example\nThis is on line 2 of a text file.\n\nThe end.'
+
+writeToFile(PATH_TO_MY_FILE, CONTENT_FOR_MY_FILE)
+
+# Run in terminal using:
+# python write-to-file.py
+#
+# It will create a file called `example.txt` and the contents will look like (minus the number signs):
+# Example
+# This is on line 2 of a text file.
+# 
+# The end.
